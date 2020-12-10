7c6eb924d0764ae96f4c1569d8fa3f86d3aa4574
tylanmm <tylanmm@byu.edu>
2020-12-10 15:20:59 -0700

    Cleaned slightly

--- advent_of_code/2020/day10/part2.py
+++ advent_of_code/2020/day10/part2.py
@@ -1,20 +1,19 @@
 import sys
 
 with open(sys.argv[1]) as f:
-    data = [0] + [int(x) for x in f.read().split('\n')]
+    data = [int(x) for x in f.read().split('\n')]
 
 data.sort()
-data.append(data[-1] + 3)
+data = [0] + data + [data[-1]+3]
 
 ways = [0]*len(data)
-ways[0] = ways[1] = 1
+ways[0] = ways[1] = ways[2] = 1
 if data[2] - data[0] <= 3:
-    ways[2] = 2
-else:
-    ways[2] = 1
+    ways[2] += 1
 
 for i in range(3, len(data)):
     for j in range(1, 4):
         if data[i] - data[i-j] <= 3:
             ways[i] += ways[i-j]
+
 print(ways[-1])
